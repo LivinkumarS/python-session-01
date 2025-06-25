ff4bcd10bbba4084338cf2f9114e448d4b60b3df
LivinkumarS <001liv100@ggmail.com>
2025-06-25 11:14:43 +0530

    Variables and Datatypes

---
+++ VariablesAndDataType.py
@@ -0,0 +1,41 @@
+# # a="Hello, welcome all"
+
+# # print(a)
+# # print(a)
+# # print(a)
+# # print(a)
+# # print(a)
+
+# num1=23
+# print(num1)
+
+# num2=22.3
+# print(num2)
+
+# str2='Livinkumar@001'
+# str1="Hello All"
+# print(str1)
+
+# str3= 'this is kumar"s home'
+# print(str3)
+
+# bool1=True
+# bool2=False
+
+# print(bool1)
+# print(bool2)
+
+# var1=56
+# var2="56"
+# var3=False
+# print(type(var3))
+
+
+# Type Casting
+# num1="67"
+# num2=int(num1)
+# print(num2)
+# print(type(num2))
+
+inp=input("Give the number: ")
+print(int(inp)+2)
