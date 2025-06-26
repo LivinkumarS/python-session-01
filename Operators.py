d8b641702dc10d0043adb2d1702b711cab68de13
LivinkumarS <001liv100@ggmail.com>
2025-06-26 12:25:30 +0530

    Operators in Python

---
+++ Operators.py
@@ -0,0 +1,40 @@
+# Arthmetic Operators
+
+# a=30
+# b=0
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# Comparison Operator
+
+# a=60
+# b=60
+
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a<=b)
+# print(a>=b)
+
+
+# Logical Operators
+# a=True
+# b=True
+
+# print(a and b)
+# print(a or b)
+# print(not a)
+
+# print((a and b) or (b and (not a)))
+
+a=10
+a+=30
+
+print(a)
