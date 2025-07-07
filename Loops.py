e302a6b795a1628979e9386e5fe6d4e622cbd88a
LivinkumarS <001liv100@ggmail.com>
2025-07-07 12:14:32 +0530

    Conditional Cls.. task

--- Loops.py
+++ Loops.py
@@ -12,7 +12,18 @@
 #     else:
 #         print("Try again")
 
-for i in range(1,101):
-    if i==50:
-        continue
-    print(i)
+marks=float(input("Enter Your Mark"))
+if 90<= marks <=100:
+    print("Grade O")
+elif 80<= marks <90:
+    print("Grade A+")
+elif 70<= marks <80:
+    print("Grade A")
+elif 60<= marks <70:
+    print("Grade B+")
+elif 50<= marks <60:
+    print("Grade B")
+elif 0<= marks <50:
+    print("Fail")
+else:
+    print("Invalid Input")
