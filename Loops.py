62fa0a0b098ed121b4b82a34067e5769b6ba0caa
LivinkumarS <001liv100@ggmail.com>
2025-07-07 11:45:25 +0530

    Loops

---
+++ Loops.py
@@ -0,0 +1,18 @@
+# for i in range(0,10):
+#     print(i)
+
+# secret=33
+# gotAnswer=False
+
+# while not gotAnswer:
+#     inp=int(input("Guess the number Clue(0-100): "))
+#     if(secret==inp):
+#         print("You won!")
+#         gotAnswer=True
+#     else:
+#         print("Try again")
+
+for i in range(1,101):
+    if i==50:
+        continue
+    print(i)
