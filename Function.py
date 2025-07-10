084a12e05c92e1a36b90715fc148da16d790695c
LivinkumarS <001liv100@ggmail.com>
2025-07-10 12:08:09 +0530

    Functions

---
+++ Function.py
@@ -0,0 +1,54 @@
+# def add(a,b):
+#     print(a+b)
+    
+# add(2,3)
+# add(34,16)
+
+# def sayHi(name):
+#     print("Hello ",name,"!")
+
+# sayHi("Livin")
+
+# def greet(name="Livin"):
+#     print("Good Morning!", name)
+    
+# greet("Saala")
+# greet()
+
+# def calc(a,b,c):
+#     print(a+b*c)
+
+# calc(2,3,4)
+# calc(c=3,b=4,a=20)
+
+# def printSteps():
+#     print("Step 1")
+#     print("Step 2")
+#     print("Step 3")
+#     return "Gift"
+#     print("Step 4")
+#     print("Step 5")
+    
+    
+# ans=printSteps()
+
+# print(ans)
+
+
+# def greet(name):
+#     return "Hello "+name+"!"
+
+# def getName(name): #Hari
+#     return greet(name)
+
+# print(getName("Hari"))
+
+a=10
+
+def add():
+    a=11
+    b=9
+    print(a+b)
+
+add()
+print(a)
