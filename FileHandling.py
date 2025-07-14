6234a457d8c79a39d83976384e8020a36cf9148f
LivinkumarS <001liv100@ggmail.com>
2025-07-14 11:07:04 +0530

    File handling

---
+++ FileHandling.py
@@ -0,0 +1,29 @@
+# file=open("Movies.txt",'w')
+# text=file.readline()
+# text=file.readline()
+# print(text)
+# file.close()
+
+# print(file)
+
+# print(file.readline())
+# print(file.readline())
+
+
+# Write
+
+# file=open("Movies.txt",'w')
+# file.write("Fast and Furius\nSpider-man\nBatman\nIron-man\nFinal Destination")
+# file.close()
+
+# Append
+
+# file=open("Movies.txt",'r+')
+# file.write("\nGhillie\nGOAT\nSarpatta Parambarai\nSachin\n96")
+# print(file.read())
+# file.close()
+
+import os
+# print(os.path.exists("mvies.txt"))
+
+os.remove("Delete.txt")
